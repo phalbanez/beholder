a2966102890662752b62a57bc6cc752275aea81b
phalbanez <phalbanez@gmail.com>
2022-05-05 19:36:23 -0300

    Added menu

--- frontend/src/private/Settings/Settings.js
+++ frontend/src/private/Settings/Settings.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { doLogout } from "../../services/AuthService";
-import { getSettings } from "../../services/SettingsService";
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Menu from '../../components/Menu/Menu';
+import { doLogout } from '../../services/AuthService';
+import { getSettings } from '../../services/SettingsService';
 
 function Settings() {
   const history = useHistory();
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   const [settings, setSettings] = useState({
-    email: "",
-    apiURL: "",
-    accessKey: "",
-    keySecret: "",
+    email: '',
+    apiURL: '',
+    accessKey: '',
+    keySecret: '',
   });
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
 
     getSettings(token)
       .then((response) => {
@@ -23,7 +24,7 @@ function Settings() {
       })
       .catch((err) => {
         if (err.response && err.response.status === 401)
-          return history.push("/");
+          return history.push('/');
 
         // console.error(err.message);
 
@@ -33,12 +34,12 @@ function Settings() {
   }, [history]);
 
   function onLogoutClick(event) {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
 
     doLogout(token)
       .then((response) => {
-        localStorage.removeItem("token");
-        history.push("/");
+        localStorage.removeItem('token');
+        history.push('/');
       })
       .catch((err) => {
         setError(err.message);
@@ -46,41 +47,44 @@ function Settings() {
   }
 
   return (
-    <main>
-      <section className="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
-        <div className="container">
-          <p className="text-center">
-            <Link to="/" className="d-flex align-items-center">
-              <svg
-                className="icon icon-xs me-2"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              {settings.email}
-            </Link>
-          </p>
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={onLogoutClick}
-          >
-            Logout
-          </button>
-          {error ? (
-            <div className="alert alert-danger">{error}</div>
-          ) : (
-            <React.Fragment></React.Fragment>
-          )}
-        </div>
-      </section>
-    </main>
+    <React.Fragment>
+      <Menu />
+      <main>
+        <section className="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
+          <div className="container">
+            <p className="text-center">
+              <Link to="/" className="d-flex align-items-center">
+                <svg
+                  className="icon icon-xs me-2"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                {settings.email}
+              </Link>
+            </p>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={onLogoutClick}
+            >
+              Logout
+            </button>
+            {error ? (
+              <div className="alert alert-danger">{error}</div>
+            ) : (
+              <React.Fragment></React.Fragment>
+            )}
+          </div>
+        </section>
+      </main>
+    </React.Fragment>
   );
 }
 
